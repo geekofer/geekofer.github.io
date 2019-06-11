4052a20ee612b844c788faf3c04e3be09d255965
Jorge Chavez <jorgecha48@gmail.com>
2019-06-11 10:43:39 -0500

    deploy

---
+++ precache-manifest.50a7b355dd5ec80d9f87fd617640bb41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1b9e2c4666c2d6f01a",
+    "url": "/geekofer/js/chunk-vendors.9fd9d84b.js"
+  },
+  {
+    "revision": "9272f4b50d94cda3c39f",
+    "url": "/geekofer/js/app.5ea2b4b2.js"
+  },
+  {
+    "revision": "864501149c5e956b9a85e1e673d3f3df",
+    "url": "/geekofer/jorge-resumen.pdf"
+  },
+  {
+    "revision": "b5a5644a692b63355cb4fc6ba2ff12db",
+    "url": "/geekofer/index.html"
+  },
+  {
+    "revision": "47fcffdb5adeebe69a7b18711d6191c7",
+    "url": "/geekofer/geekofer-photo.jpg"
+  },
+  {
+    "revision": "af1b9e2c4666c2d6f01a",
+    "url": "/geekofer/css/chunk-vendors.6ad0b682.css"
+  },
+  {
+    "revision": "9272f4b50d94cda3c39f",
+    "url": "/geekofer/css/app.a01a69b2.css"
+  }
+];
